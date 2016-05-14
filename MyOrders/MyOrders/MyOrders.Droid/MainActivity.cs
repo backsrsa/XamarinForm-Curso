84787aaae75d61b713046c44ac24ab1a303e164c
backs <jcfkbacks@gmail.com>
2016-05-14 13:27:45 -0500

    aspecto visual, iconos y colores

--- MyOrders/MyOrders/MyOrders.Droid/MainActivity.cs
+++ MyOrders/MyOrders/MyOrders.Droid/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace MyOrders.Droid
 {
-    [Activity(Label = "MyOrders", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "MyOrders", Icon = "@drawable/ic_launcher", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
