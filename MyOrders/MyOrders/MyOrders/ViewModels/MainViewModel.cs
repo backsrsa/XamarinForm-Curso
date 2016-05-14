2c3984a367938e240564e0dc85137381cc279777
backs <jcfkbacks@gmail.com>
2016-05-14 13:42:54 -0500

    Avance inicio de menu

---
+++ MyOrders/MyOrders/MyOrders/ViewModels/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOrders.ViewModels
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            LoadMenu();
+        }
+
+        private void LoadMenu()
+        {
+            Menu = new ObservableCollection<MenuItemViewModel>
+            {
+                new MenuItemViewModel()
+                {
+                    Icon = "ic_action_orders",
+                    Title = "Orders",
+                    PageName = "NewOrderPage"
+                },
+                new MenuItemViewModel()
+                {
+                    Icon = "ic_action_client",
+                    Title = "Clients",
+                    PageName = "ClientsPage"
+                },
+                new MenuItemViewModel()
+                {
+                    Icon = "ic_action_alarm",
+                    Title = "Alarm",
+                    PageName = "AlarmsPage"
+                },
+                new MenuItemViewModel()
+                {
+                    Icon = "ic_action_settings",
+                    Title = "Settings",
+                    PageName = "SettingsPage"
+                }
+            };
+        }
+
+        public ObservableCollection<MenuItemViewModel> Menu { get; set; }
+    }
+}
