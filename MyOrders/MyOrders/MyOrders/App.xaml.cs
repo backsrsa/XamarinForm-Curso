7b1dc77d3bd21d0fc04b56a9e6fb3b159ceaf542
backs <jcfkbacks@gmail.com>
2016-05-14 12:55:25 -0500

    Avance ya muestra el color rosado en la barbackground, asi como el master detail

---
+++ MyOrders/MyOrders/MyOrders/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MyOrders.Pages;
+using Xamarin.Forms;
+
+namespace MyOrders
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            // The root page of your application
+            MainPage = new MasterPage();
+        }
+
+        public static NavigationPage Navigator { get; set; }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
